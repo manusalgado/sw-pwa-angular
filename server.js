f5b8abd57feab049c27b02611e6e1f9a58f75a26
Manuel <mest.sal18@gmail.com>
2021-08-16 13:30:49 -0500

    initial heroku deploy

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/sw-pwa-angular'));
-
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/sw-pwa-angular/index.html'));
+app.use(express.static(__dirname+'/dist/sw-pwa-angular'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/dist/sw-pwa-angular/index.html'));
 });
-
-// default Heroku port
-app.listen(process.env.PORT || 5000);
